653a0c9dee2bfe5510147e82c8bf2091fce368a3
PersoNameDaniel <dmolsson@outlook.com>
2022-01-22 01:08:20 -0600

    day 2 finished, but got wrong answer.

--- Advent of Code 2021/day2.cpp
+++ Advent of Code 2021/day2.cpp
@@ -22,28 +22,33 @@ int main() {
 	}
 	else cout << "can't open this file" << endl;
 
-	
+	int variableLength;		//declacring variables
+	char distance;
+	int position = 0;
+	int depth = 0;
+
+
 	for (int arrayCounter = 0; arrayCounter <= 999; arrayCounter++) {		//for loop running once per array value
-		int variableLength = direction[arrayCounter].size();		//counting the number of characters in array value
+		variableLength = direction[arrayCounter].size();		//counting the number of characters in array value
 		if (variableLength == 4) {		//creating up if loop
-			char distance = direction[arrayCounter].at(3);		//getting the distance value from string
+			distance = direction[arrayCounter].at(3);		//getting the distance value from string
+			depth = depth - distance;		//subtracting value to depth
 			cout << "poo <up> " << distance << endl;		//debug output
 		}
 		if (variableLength == 6) {		//creating down if loop
-			char distance = direction[arrayCounter].at(5);		//getting distance value from string
+			distance = direction[arrayCounter].at(5);		//getting distance value from string
+			depth = depth + distance;		//adding value from depth
 			cout << "poo <down> " << distance << endl;		//debug output
 		}
 		if (variableLength == 9) {		//creating forward if loop
-			char distance = direction[arrayCounter].at(8);		//getting distance value from string
+			distance = direction[arrayCounter].at(8);		//getting distance value from string
+			position = position + distance;		//adding value to distance
 			cout << "poo <forward> " << distance << endl;		//debug output
 		}
-
-		
-
-	
 	}
-
-
+	cout << "position is: " << position << endl;		//debug position output
+	cout << "depth is: " << depth << endl;		//debug depth output
+	cout << "puzzle answer is: " << position * depth << endl;		//puzzle output
 
 	return 0;
 }
