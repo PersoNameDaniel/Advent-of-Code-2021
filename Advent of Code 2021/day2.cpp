395c81357956ac307947aa36fb84c9e9b4334c83
PersoNameDaniel <dmolsson@outlook.com>
2022-01-22 14:50:02 -0600

    improved commenting format, still cant find problem

--- Advent of Code 2021/day2.cpp
+++ Advent of Code 2021/day2.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main() {
-	string direction[1000];		//Recieving input (someone else's code, idk)
+	string direction[1000];		//Reading input from file (someone else's code, idk)
 	short loop = 0;
 	string line;
 	ifstream myfile("day2Input.txt");
@@ -22,33 +22,41 @@ int main() {
 	}
 	else cout << "can't open this file" << endl;
 
-	int variableLength;		//declacring variables
-	char distance;
-	int position = 0;
-	int depth = 0;
+	//Declaring variables
+	int variableLength;		//length of the selected variable within the array
+	char distance;			//distance value of the selected variable within the array
+	int position = 0;		//current position of the submarine
+	int depth = 0;			//current depth of the submarine
+	int answer;				//the final answer for problem position multiplied by depth
 
 
 	for (int arrayCounter = 0; arrayCounter <= 999; arrayCounter++) {		//for loop running once per array value
-		variableLength = direction[arrayCounter].size();		//counting the number of characters in array value
-		if (variableLength == 4) {		//creating up if loop
-			distance = direction[arrayCounter].at(3);		//getting the distance value from string
-			depth = depth - distance;		//subtracting value to depth
-			cout << "poo <up> " << distance << endl;		//debug output
+		variableLength = direction[arrayCounter].size();					//counting the number of characters in array value
+		if (variableLength == 4) {											//creating up if loop
+			distance = direction[arrayCounter].at(3);						//getting the distance value from string
+			depth = depth - distance;										//subtracting value to depth
+			cout << "<up> " << distance << endl;						//debug output
 		}
-		if (variableLength == 6) {		//creating down if loop
+		if (variableLength == 6) {							//creating down if loop
 			distance = direction[arrayCounter].at(5);		//getting distance value from string
-			depth = depth + distance;		//adding value from depth
-			cout << "poo <down> " << distance << endl;		//debug output
+			depth = depth + distance;						//adding value from depth
+			cout << "<down> " << distance << endl;			//debug output
 		}
-		if (variableLength == 9) {		//creating forward if loop
-			distance = direction[arrayCounter].at(8);		//getting distance value from string
-			position = position + distance;		//adding value to distance
-			cout << "poo <forward> " << distance << endl;		//debug output
+		if (variableLength == 9) {								//creating forward if loop
+			distance = direction[arrayCounter].at(8);			//getting distance value from string
+			position = position + distance;						//adding value to distance
+			cout << "<forward> " << distance << endl;			//debug output
+		}
+		if (variableLength != 4 && variableLength != 6 && variableLength != 9) {		//if loop checking for any values with wrong length
+			cout << "Variable Length is: " << variableLength << endl;					//outputting variable length that caused error
+			cout << "Error" << endl;													//outputting "error"
+			break;																		//stopping code
 		}
 	}
-	cout << "position is: " << position << endl;		//debug position output
-	cout << "depth is: " << depth << endl;		//debug depth output
-	cout << "puzzle answer is: " << position * depth << endl;		//puzzle output
+	answer = position * depth;										//setting answer variable to answer to probelm
+	cout << "position is: " << position << endl;					//debug position output
+	cout << "depth is: " << depth << endl;							//debug depth output
+	cout << "puzzle answer is: " << answer << endl;					//puzzle output
 
 	return 0;
 }
