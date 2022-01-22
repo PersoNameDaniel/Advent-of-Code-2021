ee8e0e80da49a261a89ea11cd3f04c76d9c6f878
PersoNameDaniel <dmolsson@outlook.com>
2022-01-21 23:59:37 -0600

    started day 2

--- Advent of Code 2021/day2.cpp
+++ Advent of Code 2021/day2.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
 
 using namespace std;
 
 int main() {
+	string direction[1000];		//Recieving input
+	short loop = 0;
+	string line;
+	ifstream myfile("day2Input.txt");
+	if (myfile.is_open()) {
+		while (!myfile.eof()) {
+			getline(myfile, line);
+			direction[loop] = line;
+			cout << direction[loop] << endl;
+			loop++;
+		}
+		myfile.close();
+		system("echo file closed");
+	}
+	else cout << "can't open this file" << endl;
+
+	
+	for (int arrayCounter = 0; arrayCounter <= 999; arrayCounter++) {		//for loop running once per array value
+		int variableLength = direction[arrayCounter].size();		//counting the number of characters in array value
+		if (variableLength == 4) {		//creating up if loop
+			char distance = direction[arrayCounter].at(3);		//getting the distance value from string
+			cout << "poo <up> " << distance << endl;		//debug output
+		}
+		if (variableLength == 6) {		//creating down if loop
+			char distance = direction[arrayCounter].at(5);		//getting distance value from string
+			cout << "poo <down> " << distance << endl;		//debug output
+		}
+		if (variableLength == 9) {		//creating forward if loop
+			char distance = direction[arrayCounter].at(8);		//getting distance value from string
+			cout << "poo <forward> " << distance << endl;		//debug output
+		}
+
+		
+
+	
+	}
+
+
 
 	return 0;
 }
